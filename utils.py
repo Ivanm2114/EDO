27e82113655040e2bcf3ea02c764380d5363f1d3
ivanm2114 <ivanm2114@mail.ru>
2021-03-07 15:01:26 +0300

    most of table is taken normaly

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
 import openpyxl
 import shutil
+from num2words import num2words
 
 
 def getContent(file):
@@ -50,11 +51,13 @@ def getContent(file):
 
 
 def putIntoXLS(data):
+    k = 13
     thin = 'thin'
     thick = 'medium'
     shutil.copyfile('ЭДО\\Schet.xlsx', 'ЭДО\\result.xlsx')
     workbook = openpyxl.load_workbook('ЭДО\\Schet.xlsx')
     sheet = workbook['TDSheet']
+
     sheet['B2'] = data['Поставщик'][0]['БанкРекв']['НаимБанк']
     sheet['AD2'] = data['Поставщик'][0]['БанкРекв']['БИК']
     sheet['AD3'] = data['Поставщик'][0]['БанкРекв']['РСчет']
@@ -81,6 +84,11 @@ def putIntoXLS(data):
         sheet.row_dimensions[row].height = 50
         sheet[f'B{row}'] = i
         sheet[f'D{row}'] = el['Название']
+        try:
+            sheet[f'AF{row}'] = el['Цена']
+        except KeyError:
+            sheet[f'AF{row}'] = el['Сумма']
+        sheet[f'AK{row}'] = el['Сумма']
 
         for letter in arr:
             if letter == 'B':
@@ -126,6 +134,92 @@ def putIntoXLS(data):
                                                     bottom=Side(border_style=thick, color='FF000000'),
                                                     left=Side(border_style=thin, color='FF000000'))
 
+    row += 2
+    for i in range(3):
+        sheet.merge_cells(f'AL{row + i}:AQ{row + i}')
+        sheet[f'AK{row + i}'].alignment = Alignment(horizontal='right',
+                                                    vertical='top',
+                                                    text_rotation=0,
+                                                    wrap_text=False,
+                                                    shrink_to_fit=False, indent=0)
+        sheet[f'AL{row + i}'].alignment = Alignment(horizontal='right',
+                                                    vertical='top',
+                                                    text_rotation=0,
+                                                    wrap_text=False,
+                                                    shrink_to_fit=False, indent=0)
+        sheet.row_dimensions[row + i].height = 15
+    sheet[f'AK{row}'] = 'Итого:'
+    sheet[f'AL{row}'] = data['ТаблДок'][0]['ИтогТабл']['Сумма']
     row += 1
+    sheet[f'AK{row}'] = 'В том числе НДС:'
+    sheet[f'AL{row}'] = data['ТаблДок'][0]['ИтогТабл']['НДС']['Сумма']
+    row += 1
+    sheet[f'AK{row}'] = 'Всего к оплате:'
+    sheet[f'AL{row}'] = data['ТаблДок'][0]['ИтогТабл']['Сумма']
+
+    row += 1
+    sheet.row_dimensions[row].height = k
+    rubles = int(sheet[f"AL{row - 1}"].value.split('.')[0])
+    cents = sheet[f"AL{row - 1}"].value.split('.')[1]
+    rubles = num2words(rubles, to='cardinal', lang='ru')
+    sheet.merge_cells(f'B{row}:AR{row}')
+    sheet[f'B{row}'] = f'Всего наименований {i} на сумму {sheet[f"AL{row - 1}"].value} руб'
+    sheet[f'B{row}'].font = Font(name='Arial',
+                                 size=9,
+                                 bold=False,
+                                 italic=False,
+                                 vertAlign=None,
+                                 underline='none',
+                                 strike=False,
+                                 color='FF000000')
+
+    row += 1
+    sheet.row_dimensions[row].height = k
+    sheet.merge_cells(f'B{row}:AP{row}')
+    sheet[f'B{row}'] = f'{rubles.capitalize()} рублей {cents} копеек'
+    sheet[f'B{row}'].font = Font(name='Arial',
+                                 size=9,
+                                 bold=True,
+                                 italic=False,
+                                 vertAlign=None,
+                                 underline='none',
+                                 strike=False,
+                                 color='FF000000')
+
+    row += 2
+    index = 0
+    text = ['Внимание!', 'Оплата данного счета означает согласие с условиями поставки товара.',
+            'Уведомление об оплате обязательно, в противном случае не гарантируется наличие товара на складе.',
+            'Товар отпускается по факту прихода денег на р/с Поставщика, самовывозом, при наличии доверенности и паспорта.']
+
+    for i in range(4):
+        sheet.row_dimensions[row + i].height = k
+        sheet.merge_cells(f'B{row + i}:AQ{row + i}')
+        sheet[f'B{row + i}'] = text[index]
+        sheet[f'B{row + i}'].font = Font(name='Arial',
+                                         size=9,
+                                         bold=False,
+                                         italic=False,
+                                         vertAlign=None,
+                                         underline='none',
+                                         strike=False,
+                                         color='FF000000')
+        sheet[f'B{row + i}'].alignment = Alignment(horizontal='left',
+                                                   vertical='top',
+                                                   text_rotation=0,
+                                                   wrap_text=True,
+                                                   shrink_to_fit=False, indent=0)
+
+        index += 1
+    row += 3
+    sheet.row_dimensions[row].height = 2*k
+    row += 1
+
+    for letter in arr + ['AQ']:
+        sheet[f'{letter}{row}'].border = Border(bottom=Side(border_style=thick, color='FF000000'))
+
+    row += 2
+
+    sheet[f]
 
     workbook.save('ЭДО\\result.xlsx')

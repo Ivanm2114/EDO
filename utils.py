bcf6b246a2bf961b091f42d54f1aaaaedc99974f
ivanm2114 <ivanm2114@mail.ru>
2021-03-07 15:14:49 +0300

    all libraries

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.styles import Border, Side, Alignment, Font
 import openpyxl
 import shutil
 from num2words import num2words
